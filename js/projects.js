7806d16a8aee27e19dc9cbeb574de88a0ed76400
samergoda <judosamer555@gmail.com>
2024-03-16 17:17:55 +0200

    'x'

--- js/projects.js
+++ js/projects.js
@@ -1,114 +1,119 @@
 const data = [
-    {name:'Game Over',
-     img:'./../images/project/gameOver.PNG',
-     tecnology:'React- react-router - axios - joi - bootstrap -SPA',
-     link:'https://game-over-react.vercel.app/',   },
+  {
+    name: "Fast Pizza React",
+    img: "./../images/project/Capture.PNG",
+    tecnology:
+      "React Router data loading, Redux, Redux Toolkit, thunks, Tailwind CSS",
+    link: "https://react-fast-pizza-samer.netlify.app/menu",
+  },
 
+  {
+    name: "Game Over",
+    img: "./../images/project/gameOver.PNG",
+    tecnology: "React- react-router - axios - joi - bootstrap -SPA",
+    link: "https://game-over-react.vercel.app/",
+  },
+  {
+    name: "React project",
+    img: "./../images/project/2.PNG",
+    tecnology:
+      "react router - axios - redux - framer motion - props -fetch API",
+    link: "https://react-project-ey7xieuf1-samergoda.vercel.app/",
+  },
+  {
+    name: "use popcorn",
+    img: "./../images/project/popcorn.PNG",
+    tecnology: "useEffect, data fetching, custom hooks",
+    link: "https://use-pop-corn-eight.vercel.app/",
+  },
 
+  {
+    name: "React Quiz",
+    img: "./../images/project/quiz.PNG",
+    tecnology: "useReducer - Components",
+    link: "https://react-quiz-blush.vercel.app/",
+  },
 
+  {
+    name: "Yummy",
+    img: "./../images/project/3.PNG",
+    tecnology: "HTML - CSS - Bootstrap - JavaScript - jQuery - fetch API",
+    link: "https://samergoda.github.io/Yummy/",
+  },
 
+  {
+    name: "Forkify",
+    img: "./../images/project/4.PNG",
+    tecnology: "HTML - CSS - JS - SASS - fetch API",
+    link: "https://forkify-samer.netlify.app/",
+  },
 
-    {name:'React project',
-     img:'./../images/project/2.PNG',
-     tecnology:'react router - axios - redux - framer motion - props -fetch API',
-     link:'https://react-project-ey7xieuf1-samergoda.vercel.app/',   },
+  {
+    name: "Arsha",
+    img: "./../images/project/5.PNG",
+    tecnology:
+      "HTML - CSS - JavaScript - tailwind - AOS animation -jQuery -dark",
+    link: "https://samergoda.github.io/Arsha-Landing-page/",
+  },
 
+  {
+    name: "Question-app-by-OOP",
+    img: "./../images/project/qes.PNG",
+    tecnology: "HTML- CSS- Bootstrap -JS",
+    link: "https://samergoda.github.io/question-app-by-OOP/",
+  },
 
+  {
+    name: "Smart Login",
+    img: "./../images/project/log.PNG",
+    tecnology: "HTML- CSS- Bootstrap -JS",
+    link: "https://samergoda.github.io/Smart-Login/",
+  },
 
-    {name:'Yummy',
-     img:'./../images/project/3.PNG',
-     tecnology:'HTML - CSS - Bootstrap - JavaScript - jQuery - fetch API',
-     link:'https://samergoda.github.io/Yummy/',   },
+  {
+    name: "Bankist",
+    img: "./../images/project/6.PNG",
+    tecnology: "HTML- CSS -JS DOM manipulation",
+    link: "https://samergoda.github.io/advanced-DOM/",
+  },
 
+  {
+    name: "Pig Game",
+    img: "./../images/project/pig.PNG",
+    tecnology: "HTML- CSS -JS DOM manipulation",
+    link: "https://samergoda.github.io/pig-game/",
+  },
 
+  {
+    name: "Weather",
+    img: "./../images/project/wes.PNG",
+    tecnology: "HTML- CSS- Bootstrap -JS",
+    link: "https://samergoda.github.io/Weather/",
+  },
 
-    {name:'Forkify',
-     img:'./../images/project/4.PNG',
-     tecnology:'HTML - CSS - JS - SASS - fetch API',
-     link:'https://forkify-samer.netlify.app/',   },
+  {
+    name: "biano",
+    img: "./../images/project/7.PNG",
+    tecnology: "html-css-js",
+    link: "https://samergoda.github.io/little-Biano-/",
+  },
 
+  {
+    name: "crud-with-react",
+    img: "./../images/project/9.PNG",
+    tecnology: "React- react-router - bootstrap ",
+    link: "https://crud-with-react-flame.vercel.app/",
+  },
 
+  // {name:'project name',
+  //  img:'./../images/project/.PNG',
+  //  tecnology:'',
+  //  link:'',   },
 
-    {name:'Arsha',
-     img:'./../images/project/5.PNG',
-     tecnology:'HTML - CSS - JavaScript - tailwind - AOS animation -jQuery -dark',
-     link:'https://samergoda.github.io/Arsha-Landing-page/',   },
-
-
-
-    {name:'Question-app-by-OOP',
-     img:'./../images/project/qes.PNG',
-     tecnology:'HTML- CSS- Bootstrap -JS',
-     link:'https://samergoda.github.io/question-app-by-OOP/',   },
-
-
-
-    {name:'Smart Login',
-     img:'./../images/project/log.PNG',
-     tecnology:'HTML- CSS- Bootstrap -JS',
-     link:'https://samergoda.github.io/Smart-Login/',   },
-
-
-
-    {name:'Bankist',
-     img:'./../images/project/6.PNG',
-     tecnology:'HTML- CSS -JS DOM manipulation',
-     link:'https://samergoda.github.io/advanced-DOM/',   },
-
-
-
-    {name:'Pig Game',
-     img:'./../images/project/pig.PNG',
-     tecnology:'HTML- CSS -JS DOM manipulation',
-     link:'https://samergoda.github.io/pig-game/',   },
-
-
-
-    {name:'Weather',
-     img:'./../images/project/wes.PNG',
-     tecnology:'HTML- CSS- Bootstrap -JS',
-     link:'https://samergoda.github.io/Weather/',   },
-
-
-
-    {name:'biano',
-     img:'./../images/project/7.PNG',
-     tecnology:'html-css-js',
-     link:'https://samergoda.github.io/little-Biano-/',   },
-
-
-    {name:'crud-with-react',
-     img:'./../images/project/9.PNG',
-     tecnology:'React- react-router - bootstrap ',
-     link:'https://crud-with-react-flame.vercel.app/',   },
-
-
-    {name:'use popcorn',
-     img:'./../images/project/popcorn.PNG',
-     tecnology:'useEffect, data fetching, custom hooks',
-     link:'https://use-pop-corn-eight.vercel.app/',   },
-
-     
-    // {name:'project name',
-    //  img:'./../images/project/.PNG',
-    //  tecnology:'',
-    //  link:'',   },
-
-    // {name:'project name',
-    //  img:'./../images/project/.PNG',
-    //  tecnology:'',
-    //  link:'',   },
-
-    // {name:'project name',
-    //  img:'./../images/project/.PNG',
-    //  tecnology:'',
-    //  link:'',   },
-
-
-
-]
-
-
+  // {name:'project name',
+  //  img:'./../images/project/.PNG',
+  //  tecnology:'',
+  //  link:'',   },
+];
 
 export default data;
-
